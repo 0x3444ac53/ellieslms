f70d4f51fd48c98944421e8935373e6940752a12
Ellie Mae Galyean <elliemaegalyean@gmail.com>
2020-02-23 07:25:43 -0500

    l

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, send_from_directory, render_template
 import pyrebase
 import json
 import requests as rq
+from urllib.parse import quote_plus
 
 firebaseConfig = {
   "apiKey": "AIzaSyCW5qlAvA2_OJkBwThVmYBx5dTclukt-KE",
@@ -29,23 +30,35 @@ def send_style_file(path):
 
 @app.route('/get_meta_data')
 def find_metadata():
-    title = request.args.get('title')
-    print(title)
-    r = rq.get(f'https://www.googleapis.com/books/v1/volumes?q=intitle:{title}')
+    params = request.args.to_dict()
+    clean_keys = list(filter(lambda x: params[x], params.keys()))
+    search_string_list = []
+    for i in clean_keys:
+        if i == 'title':
+            search_string_list.append(f"intitle:{quote_plus(params[i])}")
+        if i == 'author':
+            search_string_list.append(f"inauthor:{quote_plus(params[i])}")
+        if i == 'isbn':
+            search_string_list.append(f"isbn:{quote_plus(params[i])}")
+    r = rq.get('https://www.googleapis.com/books/v1/volumes', {
+        "q" : "+".join(search_string_list)
+    })
     results = json.loads(r.text)['items']
     results_dict = []
     for i in results:
         try:
             results_dict.append(
             {
-            "title"      : i['volumeInfo']['title'],
-            "authors"    : ", ".join(i['volumeInfo']['authors']),
-            "isbn"       : i['volumeInfo']['industryIdentifiers'][0]['identifier'],
+            "title"          : i['volumeInfo']['title'],
+            "authors"        : ", ".join(i['volumeInfo']['authors']),
+            "isbn"           : i['volumeInfo']['industryIdentifiers'][0]['identifier'],
+            "publisher"      : i['volumeInfo']['publisher'],
+            "publishedDate"  : i['volumeInfo']['publishedDate'],
             }
             )
         except KeyError:
             continue
-    return json.dumps(results_dict)
+    return json.dumps(results_dict[0])
 
 @app.route('/js/<path:path>')
 def sildnd_js(path):
@@ -55,7 +68,9 @@ def sildnd_js(path):
 def new_entry():
     if request.method == "GET":
         return send_from_directory("static", "new_entry.html")
-    db.child('books').push(request.form.to_dict())
+    formdata = request.form.to_dict()
+    db.child('books').push(formdata)
+    return "<h1>Success</h1><br><a id='again' href='/new_entry'>Submit another?</a>"
 
 
 if __name__ == "__main__":

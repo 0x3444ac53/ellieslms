49b4d4b01688564a0d4f25d33cf0303ab36370ea
Ellie Mae Galyean <elliemaegalyean@gmail.com>
2020-02-22 21:34:04 -0500

    added endpoint for getting book info

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, send_from_directory, render_template
 import pyrebase
+import json
+import requests as rq
 
 firebaseConfig = {
   "apiKey": "AIzaSyCW5qlAvA2_OJkBwThVmYBx5dTclukt-KE",
@@ -23,13 +25,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    print("test")
-    print(users.val())
-    return "<h1>Hello Wolrd</h1>"
+    return send_from_directory('static/landing_page.html')
 
 @app.route('/styles/<path:path>')
 def send_style_file(path):
     return send_from_directory('static/styles', path)
+
+@app.route('/get_meta_data')
+def find_metadata():
+    title = request.args.get('title')
+    author = request.args.get('author')
+    r = rq.get(f'https://www.googleapis.com/books/v1/volumes?q=')
+    results = json.loads(r.text)['items'][:10]
+    results_dict = [
+        {
+            "title"      : i['volumeInfo']['title'], 
+            "authors"    : ", ".join(i['volumeInfo']['authors']), 
+            "isbn"       : i['volumeInfo']['industryIdentifiers'][0]['identifier'],
+            "publisher"  : i['volumeInfo']['publisher'],
+            "publishDate": i["volumeInfo"]['publishedDate']
+        } for i in results
+    ]
+    return json.dumps(results_dict)
 
 @app.route('/js/<path:path>')
 def send_js(path):
